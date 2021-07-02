23d6d1cf2e34b0342e20decba0f3d528051fe6ec
GAUTAMMISTRY <gautammistry48@gmail.com>
2021-07-02 22:32:22 +0530

    first commit

---
+++ vaccine_verify_api/server.js
@@ -0,0 +1,77 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+ var MongoClient = require('mongodb').MongoClient,
+    test = require('assert');
+  // Connection url
+  var url = 'mongodb+srv://gautam:dbecommerce@masterapi.crulm.mongodb.net/vaccinedb';
+  // Connect using MongoClient
+  MongoClient.connect(url, function(err, db) {
+    // Get an additional db
+    db.close();
+  });
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/contacts", function(req, res) {
+});
+
+app.post("/contacts", function(req, res) {
+});
+
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/contacts/:id", function(req, res) {
+});
+
+app.put("/contacts/:id", function(req, res) {
+});
+
+app.delete("/contacts/:id", function(req, res) {
+});
+
+
+app.post("/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+// CONTACTS API ROUTES BELOW
